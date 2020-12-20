e36f77895197c6894b746d1aaef366e36dce9db2
carollia99 <carollia@usc.edu>
2020-12-19 23:48:04 -0500

    popups and fading for artwork

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ $(window).scroll(function(){
   }
  });
 
+//navbar visual controls
 $(document).on('click', '.nav-item', function(){ 
     $('.nav-item').removeClass("active bg-dark");
     $('.nav-item > a').removeClass("text-light");
     $(this).addClass('active bg-dark');
     $(this).find('a').addClass("text-light");
+});
+
+//pop-up modal for artwork
+$(document).on("click", '.art-image', function() {
+    $('#show-img').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
 });
